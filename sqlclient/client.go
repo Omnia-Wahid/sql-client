d5f6a818a5fd95b0f97872be5e3dc27aa8a724bf
omnia <owahid@iotblue.net>
2021-09-22 13:01:27 +0200

    sql

---
+++ sqlclient/client.go
@@ -0,0 +1,35 @@
+package sqlclient
+
+import (
+	"database/sql"
+	"errors"
+)
+
+type client struct {
+	db *sql.DB
+}
+
+type row struct {
+
+}
+type sqlClient interface{
+	Query(query string, args ...interface{}) (*row, error)
+}
+
+func Open(driverName, dataSourceName string) (sqlClient, error){
+	if driverName==""{
+		return nil, errors.New("invalid driver name")
+	}
+	db, err := sql.Open(driverName, dataSourceName)
+	if err!=nil{
+		return nil, err
+	}
+	client:= &client{
+		db: db,
+	}
+	return client,err
+}
+
+func(c *client)Query(query string, args ...interface{}) (*row, error){
+	return nil,nil
+}
